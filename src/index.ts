d10f9d19528796f27d65fc8925c255c17f6982bc
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2018-06-07 17:02:37 -0500

    Reading JSON for active notebook using contentmanager

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
+
+import {
+    INotebookTracker
+} from '@jupyterlab/notebook';
+
+import {
+  ContentsManager
+} from '@jupyterlab/services';
+
 import {
   JupyterLab,
   JupyterLabPlugin
 } from '@jupyterlab/application';
 
 import '../style/index.css';
+
+// Activate the jupyterhub extension.
+function activateExtension(app: JupyterLab, notebooks: INotebookTracker): void {
+    const tracker = notebooks;
+    tracker.currentChanged.connect(() =>
+    {
+      if (tracker.currentWidget) {
+        let path = tracker.currentWidget.context.path;
+        console.log('path = ' + path);
+        let slashIndex = path.lastIndexOf('/');
+        let directory = ''
+        if (slashIndex != -1) {
+          directory = path.substring(0, slashIndex + 1);
+        }
+        console.log('directory = ' + directory);
+        let contents = new ContentsManager();
+        contents.get(directory + '.all_of_us_config.json').then(
+              (model) => {
+                console.log('json: ', model.content);
+              }
+        );
+
+      } else {
+        console.log('No notebook selected.');
+      }
+    });
+    console.log('Hello World 2!');
+}
 
 
 /**
@@ -12,10 +49,9 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab_allofus',
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    /* TODO: Replace this with real stuff */
-    console.log('Hello World');
-  }
+  activate: activateExtension,
+  requires: [INotebookTracker]
 };
 
 export default extension;
+
